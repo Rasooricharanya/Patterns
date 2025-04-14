52cc55b8edcdc4e22167c316f90b284ce528d991
Rasooricharanya <128916473+Rasooricharanya@users.noreply.github.com>
2025-04-14 12:03:17 +0530

    Add files via upload

---
+++ floydstri.java
@@ -0,0 +1,17 @@
+public class floydstri {
+    public static void floydTriangle(int n){
+        int num = 1;
+        for(int i=1;i<=n;i++){
+            for(j=i;j<=i;j++){
+                System.out.print(num);
+                num++;
+            }
+            System.out.println();
+        }
+    }
+public static void main(String[] args){
+    int n=5;
+    floydTriangle(n);
+    }
+}
+
---
+++ halfPyrWitnNum.java
@@ -0,0 +1,10 @@
+public class halfPyrWitnNum {
+    public static void main(String[] args){
+        int num = 5;
+        int rows = 1
+        for(int i=1;i<=rows;i+=5){
+            for(int j=)
+        }
+    }
+    
+}
